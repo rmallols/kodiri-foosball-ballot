e5c2a387d3993adf521dc430f1f72a733491f199
Ricardo Mallols <ricardo.mallols@gmail.com>
2018-07-16 19:21:43 +0100

    Add Zoltan and Raj

--- src/teams.js
+++ src/teams.js
@@ -1,11 +1,11 @@
 export default function getTeams() {
     return [
-        { country: 'England', members: ['Aaron'] },
+        { country: 'England', members: ['Aaron', 'Zoltan'] },
         { country: 'China & Taiwan', members: ['Justin', 'Albert'] },
         { country: 'Spain', members: ['Chris', 'Ricardo'] },
         { country: 'Ukraine', members: ['Constantin', 'Cristian'] },
         { country: 'Wales', members: ['Dave'] },
-        { country: 'Brazil', members: ['Damany'] },
+        { country: 'Brazil', members: ['Damany', 'Raj'] },
         { country: 'Slovenia', members: ['Vesna'] },
         { country: 'Italy', members: ['Vicky'] }
     ];
