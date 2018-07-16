534cd78670a2aae80ee0ba1a8b74192f753223a8
Ricardo Mallols <ricardo.mallols@gmail.com>
2018-07-16 18:29:30 +0100

    Init team data model

---
+++ src/teams.js
@@ -0,0 +1,51 @@
+export default function getTeams() {
+    return [{
+        name: 'England',
+        members: [
+            { name: 'Aaron', origin: 'Manchester' },
+            { name: '???', origin: '???' }
+        ]
+    }, {
+        name: 'China & Taiwan',
+        members: [
+            { name: 'Justin', origin: 'Hong Kong' },
+            { name: 'Albert', origin: 'Taiwan' }
+        ]
+    }, {
+        name: 'Spain',
+        members: [
+            { name: 'Chris', origin: 'Barcelona' },
+            { name: 'Ricardo', origin: 'Valencia' }
+        ]
+    }, {
+        name: 'Ukraine',
+        members: [
+            { name: 'Constantin', origin: 'Kiev' },
+            { name: 'Cristian', origin: '???' }
+        ]
+    }, {
+        name: 'Wales',
+        members: [
+            { name: 'Dave', origin: 'Newport' },
+            { name: '???', origin: '???' }
+        ]
+    }, {
+        name: 'Brasil',
+        members: [
+            { name: 'Damany', origin: 'Sao Paolo' },
+            { name: 'Raj', origin: 'Glasgow' }
+        ]
+    }, {
+        name: 'Slovenia',
+        members: [
+            { name: 'Vesna', origin: 'Ljubljana' },
+            { name: '???', origin: '???' }
+        ]
+    }, {
+        name: 'Italy',
+        members: [
+            { name: 'Viky', origin: 'Florence' },
+            { name: '???', origin: '???' }
+        ]
+    }];
+}
